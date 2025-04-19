fa2e1b8160d7b176309a0343875e4cbdfca57528
leeyn <leeyn.shun@gmail.com>
2025-04-19 17:51:44 +0300

    Change tester to handle async

--- tester.js
+++ tester.js
@@ -1,22 +1,29 @@
-export function tester(tests){
-    tests.forEach((testFn, i) => {
-        try {
-            const fnSource = testFn.toString();
-            testFn({
-                eq: (a, b = "") => {
-                    const passed = JSON.stringify(a) === JSON.stringify(b);
-                    if (!passed) {
-                        throw new Error(`
-    case:     ${fnSource}
-    expected: ${JSON.stringify(b)}
-    actual:   ${JSON.stringify(a)}`);
-                    }
-                    return true;
-                }
-            });
-            console.log(`✅ Test ${i + 1} bimmed`);
-        } catch (err) {
-            console.error(`❌ Test ${i + 1} failed:`, err.message);
+export async function tester(tests) {
+    for (let i = 0; i < tests.length; i++) {
+      const testFn = tests[i];
+      try {
+        const fnSource = testFn.toString();
+        const result = await testFn({
+          eq: (a, b = "") => {
+            const passed = JSON.stringify(a) === JSON.stringify(b);
+            if (!passed) {
+              throw new Error(`
+  case:     ${fnSource}
+  expected: ${JSON.stringify(b)}
+  actual:   ${JSON.stringify(a)}`);
+            }
+            return true;
+          }
+        });
+  
+        if (result === false) {
+          throw new Error(`Returned false`);
         }
-    });
-}
+  
+        console.log(`✅ Test ${i + 1} passed`);
+      } catch (err) {
+        console.error(`❌ Test ${i + 1} failed:`, err.message);
+      }
+    }
+  }
+  

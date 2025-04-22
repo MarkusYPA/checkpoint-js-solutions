6a4d7700c9035f1056c02e6090f728688c39f568
Markus laptop <markus.amberla@gritlab.ax>
2025-04-22 16:11:47 -0400

    all level 5

--- group5/js-sleep-breaker/js-sleep-breaker.js
+++ group5/js-sleep-breaker/js-sleep-breaker.js
@@ -1,1 +1,23 @@
 
+// sleepBreaker returns a  promise that resolves with undefined
+// when either the delay is reached or breaker resolves
+
+export function sleepBreaker(delay, breaker) {
+
+    // new promise has resolve and reject as parameters
+    return new Promise((resolve) => {
+
+        // define a timer to resolve after delay
+        // starts right away
+        const to = setTimeout(() => {
+            resolve();
+        }, delay);
+
+        // start breaker
+        breaker().then(() => {
+            // cancel the sleep timer and resolve
+            clearTimeout(to); 
+            resolve();
+        });
+    });
+}
--- group5/js-zoo-race/js-zoo-race.js
+++ group5/js-zoo-race/js-zoo-race.js
@@ -1,1 +1,27 @@
 
+export async function animal(
+    name,
+    maxSpeed,
+    maxSpeedRange,
+    midSpeed,
+    midSpeedRange,
+    speed,
+    distance,
+) {
+    let time = 0;
+    if (distance <= maxSpeedRange) {
+        time = distance / maxSpeed;
+    } else if (distance <= maxSpeedRange + midSpeedRange) {
+        time = maxSpeedRange / maxSpeed + (distance - maxSpeedRange) / midSpeed;
+    } else {
+        time = maxSpeedRange / maxSpeed + midSpeedRange / midSpeed + (distance - maxSpeedRange - midSpeedRange) / speed;
+    }
+
+    return new Promise((res) => {
+        setTimeout(() => res(name), time)    // arrow function inside timeout
+    })
+}
+
+export function zooRace(animals) {
+    return Promise.race(animals);
+}

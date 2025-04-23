d30138ef2029ddee95830bf94b587efe7174cdb7
Markus laptop <markus.amberla@gritlab.ax>
2025-04-23 16:39:10 -0400

    more level 6

--- group6/js-deep-find/js-deep-find.js
+++ group6/js-deep-find/js-deep-find.js
@@ -1,1 +1,22 @@
 
+export function deepFind(obj, path) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+        throw new Error("Input must be a non-null object");
+    }
+    if (typeof path !== 'string') {
+        throw new Error("Path must be a string");
+    }
+
+    const steps = path.split('.');
+    let thisLevel = obj;
+
+    for (const step of steps) {
+        if (Object.keys(thisLevel).includes(step)) { // works for arrays too
+            thisLevel = thisLevel[step];
+        } else {
+            return undefined
+        }
+    }
+
+    return thisLevel;
+}
--- group6/js-deep-freeze/js-deep-freeze.js
+++ group6/js-deep-freeze/js-deep-freeze.js
@@ -1,1 +1,19 @@
 
+function freezeIt(obj){
+    Object.freeze(obj);
+    for (const val of Object.values(obj)) {
+        if (typeof val === 'object' && val !== null && val !== obj) { // last clause deals with circular reference
+            freezeIt(val);
+        }
+    }
+}
+
+export function deepFreeze(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        throw new Error ("Input must be a non-null object")
+    }
+
+    freezeIt(obj);    
+
+    return obj;
+}

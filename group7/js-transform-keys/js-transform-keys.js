d67706710783b776b66aad18e0c89575b7722ea6
Markus laptop <markus.amberla@gritlab.ax>
2025-04-23 17:08:21 -0400

    one left on level 7

--- group7/js-transform-keys/js-transform-keys.js
+++ group7/js-transform-keys/js-transform-keys.js
@@ -1,1 +1,17 @@
 
+function tk(obj, transformFn) {
+    for (const [key, val] of Object.entries(obj)) {
+        // transform keys of sub objects first
+        if (typeof val === 'object' && val !== null && !Array.isArray(val)) {
+            tk(val, transformFn);  
+        }
+        // assign value to new key and delete the old one
+        obj[transformFn(key)] = val;
+        delete obj[key];
+    }
+}
+
+export function transformKeys(obj, transformFn) {
+    tk(obj, transformFn);   // use non-returning function instead of recurring this one
+    return obj;
+}

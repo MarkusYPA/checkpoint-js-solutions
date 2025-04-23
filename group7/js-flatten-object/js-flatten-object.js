974247928416424ac1cf80995fe9f76476f4f6b1
Markus laptop <markus.amberla@gritlab.ax>
2025-04-23 09:28:58 -0400

    group 7 first

--- group7/js-flatten-object/js-flatten-object.js
+++ group7/js-flatten-object/js-flatten-object.js
@@ -1,1 +1,58 @@
 
+export function flattenObject(obj) {
+    const flatObj = {};
+
+    // Define two functions inside here: one for flattening arrays, the other for flattening objects
+    // Each can call the other or itself
+    // Both modify the flatObj -object that will be returned
+    // All test cases are run with an object -> start with object flattening
+
+    const flatArr = (arr, path) => {
+        arr.forEach((ele, i) => {
+            const arrKey = [path, String(i)].join('.');
+
+            // primitive
+            if (typeof ele !== 'object' || ele === null) {
+                flatObj[arrKey] = ele;
+                return;
+            }
+
+            // arrays
+            if (Array.isArray(ele)) {
+                flatArr(ele, arrKey);
+                return;
+            }
+
+            // object
+            flatten(ele, arrKey);
+        })
+    }
+
+    const flatten = (obj, path = "") => {
+        for (const [k, v] of Object.entries(obj)) {
+
+            let key = k;
+            if (path !== "") {
+                key = [path, k].join('.')
+            }
+
+            // primitive
+            if (typeof v !== 'object' || v === null) {
+                flatObj[key] = v;
+                continue;
+            }
+
+            // arrays
+            if (Array.isArray(v)) {
+                flatArr(v, key);
+                continue;
+            }
+
+            // object
+            flatten(v, key);
+        }
+    }
+
+    flatten(obj);
+    return flatObj;
+}
